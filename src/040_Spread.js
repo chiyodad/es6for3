5df8313a7483375cfa6196907d94d85d73890263
hika, maeng <hika00@gmail.com>
2016-05-17 10:20:28 +0900

    Create 040_Spread.js

---
+++ src/040_Spread.js
@@ -0,0 +1,13 @@
+if(!window.Spread || bsTest)(function(){
+	var sp = function(r, a){
+			for(var i = 0, j = a.length; i < j; i++){
+				if(a[i] == '...') sp(r, a[++i]);
+				else r[r.length] = a[i];
+			}
+		};
+	window.Spread =  function(a){
+		var r = [];
+		sp(r, arguments.length > 1 ? arguments : a);
+		return r;
+	};
+})();
