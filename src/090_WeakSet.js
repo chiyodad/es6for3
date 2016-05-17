370e93e0c2b40f4592648481ace60b686228abb4
hika, maeng <hika00@gmail.com>
2016-05-17 14:24:30 +0900

    Update 090_WeakSet.js

--- src/090_WeakSet.js
+++ src/090_WeakSet.js
@@ -1,20 +1,16 @@
 if(!window.WeakSet || bsTest) (function(){
-	var SET = Symbol(),
+	var SET = Symbol(), ID = Symbol(),
+		type = {'object':1, 'function':1},
+		check = function(k){return k && type[typeof k];},
 		WSet = function(a){
-			var cnt = 10000;
+			var self = this;
 			this[SET] = [];
-			if(typeof a[Symbol.iterator] === 'function') a = a[Symbol.iterator]();
-			else if(typeof a.next !== 'function') return;
-			do {
-				v = a.next();
-				if(v.done) break;
-				this.add(v.value);
-			} while(cnt--);
+			if(a)For.Of(a, function(v){self.add(v);});
 			Object.freeze(this);
 		},
 		fn = WSet.prototype;
-	fn.has = function(v){return this[SET].indexOf(v) !== -1;};
-	fn.add = function(v){if(!this.has(v) && v instanceof Object) this[SET].push(v);};
+	fn.has = function(v){return check(v) ? this[SET].indexOf(v) > -1 : false;};
+	fn.add = function(v){if(!this.has(v)) this[SET].push(v);};
 	fn['delete'] = function(v){if(this.has(v)) this[SET].splice(this[SET].indexOf(v), 1);};
 	window.WeakSet = WSet;
 })();
