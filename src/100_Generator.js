e882b0af9d6f6c69e114f92106545cac1b41d81c
hika, maeng <hika00@gmail.com>
2016-05-17 10:23:01 +0900

    Create 100_Generator.js

---
+++ src/100_Generator.js
@@ -0,0 +1,9 @@
+if(!window.Generator || bsTest) window.Generator = function(f){
+	return function(){
+		var r = [];
+		window.Yield = function(v){r[r.length] = v;};
+		f.apply(null, arguments);
+		delete window.Yield;
+		return new Iter(r);
+	};
+};
