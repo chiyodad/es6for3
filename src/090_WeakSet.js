d1e370aff20b42164f0497504ad90a75474decba
kevin(정재남) <kevinj@coupang.com>
2016-05-17 14:21:44 +0900

    WeakSet

--- src/090_WeakSet.js
+++ src/090_WeakSet.js
@@ -1,1 +1,20 @@
-
+if(!window.WeakSet || bsTest) (function(){
+	var SET = Symbol(),
+		WSet = function(a){
+			var cnt = 10000;
+			this[SET] = [];
+			if(typeof a[Symbol.iterator] === 'function') a = a[Symbol.iterator]();
+			else if(typeof a.next !== 'function') return;
+			do {
+				v = a.next();
+				if(v.done) break;
+				this.add(v.value);
+			} while(cnt--);
+			Object.freeze(this);
+		},
+		fn = WSet.prototype;
+	fn.has = function(v){return this[SET].indexOf(v) !== -1;};
+	fn.add = function(v){if(!this.has(v) && v instanceof Object) this[SET].push(v);};
+	fn['delete'] = function(v){if(this.has(v)) this[SET].splice(this[SET].indexOf(v), 1);};
+	window.WeakSet = WSet;
+})();
