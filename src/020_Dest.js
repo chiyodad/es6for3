a2e7e2b2625cce580682e3e36d1857cbec8a5eef
chiyodad <chiyodad@yahoo.com>
2016-05-19 02:10:37 +0900

    Update 020_Dest.js

--- src/020_Dest.js
+++ src/020_Dest.js
@@ -4,33 +4,33 @@ if(!window.Dest || bsTest)(function(){
 			getVal, getData, parse, destructuring, Var, Dest,
 			arr = [], obj = [], a, o, ad, od, at, ot,
 			rObj = /(\{[^\{\[\]\}]*\})/g, rArr = /(\[[^\{\[\]\}]*\])/g, 
-			oR = function(v){return ot[o] = v, '@o_'+ od +'_' + (o++) + '@';},
-			aR = function(v){return at[a] = v, '@a_'+ ad +'_' + (a++) + '@';},
+			oR = function(v){return ot[o] = v, '@o_'+ od +'_' + (o++) + '@';}, //object replace -> ot : ot[o] = '{c}'
+			aR = function(v){return at[a] = v, '@a_'+ ad +'_' + (a++) + '@';}, //
 			rO = /(@o_[^@]+@)/g, rA = /(@a_[^@]+@)/g, rR = /^(@o_[^@]+@|@a_[^@]+@)$/,
 			rNum = /^[-]?[.0-9]+$/, rStr = /^('[^']*'|"[^"]*")$/,
 			primi = {'true':true, 'false':false, 'null':null};
 		getData = function(d){
 			var target = d.search(rO) > -1 ? obj : d.search(rA) > -1 ? arr : 0;
-			if(target) return d = d.substring(1, d.length - 1).split('_'), target[d[1]][d[2]];
+			if(target) return d = d.substring(1, d.length - 1).split('_'), target[d[1]][d[2]]; //obj, arr 중 
 			return false;
 		};
 		getVal = function(d){
 			var target = d.search(rO) > -1 ? obj : d.search(rA) > -1 ? arr : 0;
 			if(target) return d = d.substring(1, d.length - 1).split('_'), JSON.parse(target[d[1]][d[2]]);
-			else if(d.search(rStr) > -1) return d.substring(1, d.length - 1);
-			else if(d.search(rNum) > -1) return parseFloat(d);
-			else if(d = primi[d]) return d;
+			else if(d.search(rStr) > -1) return d.substring(1, d.length - 1); //문자열 처리
+			else if(d.search(rNum) > -1) return parseFloat(d); // parseFloat : 011 처리 
+			else if(d = primi[d]) return d; //primi - 11번
 		};
-		Dest = function(dest){
-			var loop, r = this[DEST] = {};
-			arr.length = obj.length = a = o = ad = od = 0, dest = dest.trim();
+		Dest = function(dest){  //Dest 클래스 생성
+			var loop, r = this[DEST] = {};  // r- DEST 저장용
+			arr.length = obj.length = a = o = ad = od = 0, dest = dest.trim(); // 초기화, dest 값 trim()
 			do{
-				loop = 0;
-				if(dest.search(rObj) > -1) obj[od] = ot = [], dest = dest.replace(rObj, oR), od++, loop = 1;
+				loop = 0; //재귀를 루프로 만들기 위해 만든 스택 - obj, a
+				if(dest.search(rObj) > -1) obj[od] = ot = [], dest = dest.replace(rObj, oR), od++, loop = 1;//rObj-6번줄
 				if(dest.search(rArr) > -1) arr[ad] = at = [], dest = dest.replace(rArr, aR), ad++, loop = 1;
 			}while(loop);
-			if(dest.indexOf('=') > -1) dest = dest.split('='), r[DEFAULT] = getVal(dest[1].trim()), dest = dest[0].trim();
-			if(dest.search(rR) == -1) throw 'invalid destructuring';
+			if(dest.indexOf('=') > -1) dest = dest.split('='), r[DEFAULT] = getVal(dest[1].trim()), dest = dest[0].trim();//getVal - 17
+			if(dest.search(rR) == -1) throw 'invalid destructuring'; //잘못된 해체 - obj 나 arr이 아님
 			parse(dest, r);
 		};
 		Dest.prototype.value = function(v){
@@ -44,11 +44,11 @@ if(!window.Dest || bsTest)(function(){
 			this.k = k;
 		};
 		Var.prototype.toString = function(){return this.k;};
-		parse = function(dest, r){
+		parse = function(dest, r){ //파싱
 			var v, isObj;
 			dest = dest.trim();
 			if(v = getData(dest)){
-				isObj = v.charAt(0) == '{' ? 1 : 0;
+				isObj = v.charAt(0) == '{' ? 1 : 0; //obj 확인
 				v.substring(1, v.length - 1).split(',').forEach(function(v, idx){
 					var p;
 					v = v.trim();
@@ -64,7 +64,7 @@ if(!window.Dest || bsTest)(function(){
 		destructuring = function(target, v, result){
 			var k, key;
 			for(k in target){
-				key = target[k];
+				key = target[k]; //파싱된 객체 트리를 이용해서 객체 트리를 실행함.
 				if(key instanceof Var) result[key] = v[k] === undefined ? key[DEFAULT] : v[k];
 				else if(key && typeof key == 'object') destructuring(key, v[k], result);
 			}
